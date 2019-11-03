3f18253589a3754cf5a8833ea972b538cfdde4cd
That_Guy <eghove@gmail.com>
2019-11-03 11:06:11 -0600

    saving progress sans firebase config

--- socialape-functions/functions/index.js
+++ socialape-functions/functions/index.js
@@ -3,44 +3,139 @@ const admin = require("firebase-admin");
 
 admin.initializeApp();
 
-const express = require('express');
+const express = require("express");
 const app = express();
+const dotenv = require('dotenv');
+dotenv.config()
 
-app.get('/screams', (req, res) => {
-  admin
-  .firestore()
-  .collection("screams")
-  .get()
-  .then(data => {
-    let screams = [];
-    data.forEach(doc => {
-      screams.push(doc.data());
-    });
-    return res.json(screams);
-  })
-  .catch(err => console.error(err));
+
+
+// Initialize Firebase
+const firebase = require("firebase");
+firebase.initializeApp(firebaseConfig);
+
+const db = admin.firestore();
+
+app.get("/screams", (req, res) => {
+  db.collection("screams")
+    .orderBy("createdAt", "desc")
+    .get()
+    .then(data => {
+      let screams = [];
+      data.forEach(doc => {
+        screams.push({
+          screamId: doc.id,
+          body: doc.data().body,
+          userHandle: doc.data().userHandle,
+          createdAt: doc.data().createdAt
+        });
+      });
+      return res.json(screams);
+    })
+    .catch(err => console.error(err));
 });
 
-app.post('/scream', (req, res) => {
-  if(req.method !== 'POST') {
-    return res.status(400).json({ error: 'Method not allowed'});
+app.post("/scream", (req, res) => {
+  if (req.method !== "POST") {
+    return res.status(400).json({ error: "Method not allowed" });
   }
   const newScream = {
     body: req.body.body,
     userHandle: req.body.userHandle,
-    createdAt: admin.firestore.Timestamp.fromDate(new Date())
+    createdAt: new Date().toISOString()
   };
 
-  admin.firestore()
-    .collection('screams')
+  db.collection("screams")
     .add(newScream)
     .then(doc => {
-      res.json( { message: `document ${doc.id} created successfully`})
+      res.json({ message: `document ${doc.id} created successfully` });
     })
-    .catch( err => {
-      res.status(500).json({ error: 'something went wrong'});
+    .catch(err => {
+      res.status(500).json({ error: "something went wrong" });
       console.error(err);
-    })
+    });
 });
 
-exports.api = functions.https.onRequest(app); 
+// building out a helper functions
+const isEmpty = (string) => {
+  if(string.trim() === '') return true;
+  else return false;
+};
+
+const isEmail = (email) => {
+  const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  if(email.match(regEx)) return true;
+  else return false;
+}
+// signup route
+app.post("/signup", (req, res) => {
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    confrimPassword: req.body.confrimPassword,
+    handle: req.body.handle
+  };
+
+  let errors = {};
+
+  if(isEmpty(newUser.email)) {
+    errors.email = 'Must not be empty!'
+  } else if (!isEmail(newUser.email)) {
+    errors.email = "Must be valid email address"
+  }
+
+  if(isEmpty(newUser.password)) {
+    errors.password = "Must not be empty"
+  }
+  if (newUser.password !== newUser.confrimPassword) {
+    errors.confrimPassword = "Passwords must match"
+  }
+  if(isEmpty(newUser.handle)) {
+    errors.handle = "Must not be empty"
+  }
+  if(Object.keys(errors).length > 0) {
+    return res.status(400).json(errors);
+  }
+
+  // TODO: VALIDATE DATA
+  let token, userId;
+  db.doc(`/users/${newUser.handle}`)
+    .get()
+    .then(doc => {
+      if (doc.exists) {
+        return res.status(400).json({ handle: "this handle is already taken" });
+      } else {
+        return firebase
+          .auth()
+          .createUserWithEmailAndPassword(newUser.email, newUser.password);
+      }
+    })
+    .then(data => {
+      userId = data.user.uid;
+      return data.user.getIdToken();
+    })
+    .then(idToken => {
+      token = idToken;
+      // return res.status(201).json({ token });
+      const userCredentials = {
+        handle: newUser.handle,
+        email: newUser.email,
+        createdAt: new Date().toISOString(),
+        userId: userId
+      };
+      return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+    })
+    .then(() => {
+      return res.status(201).json({ token });
+    })
+    .catch(err => {
+      console.error(err);
+      if (err.code === "auth/email-already-in-use") {
+        return res.status(400).json({ email: "Email is already in use" });
+      } else {
+        return res.status(500).json({ error: err.code });
+      }
+    });
+});
+
+exports.api = functions.https.onRequest(app);
