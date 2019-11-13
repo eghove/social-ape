08392c56197f3d27903104d4ad531ceef4d50970
That_Guy <eghove@gmail.com>
2019-11-13 15:09:54 -0600

    delete a scream routing

--- socialape-functions/functions/index.js
+++ socialape-functions/functions/index.js
@@ -1,33 +1,43 @@
 const functions = require("firebase-functions");
 
-
-
-
 const express = require("express");
 const app = express();
-const FBAuth = require('./util/FBAuth');
-const { getAllScreams, postOneScream, getScream, commentOnScream } = require('./handlers/screams');
-const { signUp, login, uploadImage, addUserDetails, getAuthenticatedUser } = require('./handlers/users');
+const FBAuth = require("./util/FBAuth");
+const {
+  getAllScreams,
+  postOneScream,
+  getScream,
+  commentOnScream,
+  likeScream,
+  unlikeScream,
+  deleteScream
+} = require("./handlers/screams");
+const {
+  signUp,
+  login,
+  uploadImage,
+  addUserDetails,
+  getAuthenticatedUser
+} = require("./handlers/users");
 
 const dotenv = require("dotenv");
 dotenv.config();
-
-
-
-
 
 // SCREAM ROUTES
 // get screams
 app.get("/screams", getAllScreams);
 // post a scream
 app.post("/scream", FBAuth, postOneScream);
-// get 
-app.get('/scream/:screamId', getScream);
+// get
+app.get("/scream/:screamId", getScream);
 // TODO: delete scream
-// TODO: like a scream
-// TODO unlike a scream
-// TODO comment on scream
-app.post('/scream/:screamId/comment', FBAuth, commentOnScream)
+app.delete('/scream/:screamId', FBAuth, deleteScream);
+// like a scream
+app.get("/scream/:screamId/like", FBAuth, likeScream);
+// unlike a scream
+app.get("/scream/:screamId/unlike", FBAuth, unlikeScream);
+// comment on scream
+app.post("/scream/:screamId/comment", FBAuth, commentOnScream);
 
 // USER ROUTES
 // signup route
@@ -35,13 +45,10 @@ app.post("/signup", signUp);
 // login route
 app.post("/login", login);
 // image route
-app.post('/user/image', FBAuth, uploadImage);
+app.post("/user/image", FBAuth, uploadImage);
 // route to add user details
-app.post('/user', FBAuth, addUserDetails);
+app.post("/user", FBAuth, addUserDetails);
 // get credentials route
-app.get('/user', FBAuth, getAuthenticatedUser)
-
-
-
+app.get("/user", FBAuth, getAuthenticatedUser);
 
 exports.api = functions.https.onRequest(app);
