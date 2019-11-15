f1daf1325105aa154aa46fb49b3f90f1a3d31b25
That_Guy <eghove@gmail.com>
2019-11-15 16:40:47 -0600

    starting on the front end

---
+++ socialape-client/src/App.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+// Components
+import Navbar from "./components/Navbar";
+
+// Pages
+import home from "./pages/home";
+import login from "./pages/login";
+import signup from "./pages/signup";
+
+const theme = createMuiTheme({
+  palette: {
+    common: { black: "#000", white: "#fff" },
+    background: { paper: "#fff", default: "#fafafa" },
+    primary: {
+      light: "#7986cb",
+      main: "rgba(126, 211, 33, 1)",
+      dark: "#303f9f",
+      contrastText: "#fff"
+    },
+    secondary: {
+      light: "#ff4081",
+      main: "#f50057",
+      dark: "#c51162",
+      contrastText: "#fff"
+    },
+    error: {
+      light: "#e57373",
+      main: "#f44336",
+      dark: "#d32f2f",
+      contrastText: "#fff"
+    },
+    text: {
+      primary: "rgba(0, 0, 0, 0.87)",
+      secondary: "rgba(0, 0, 0, 0.54)",
+      disabled: "rgba(0, 0, 0, 0.38)",
+      hint: "rgba(0, 0, 0, 0.38)"
+    }
+  }
+});
+
+function App() {
+  return (
+    <MuiThemeProvider theme={theme}>
+      <div className="App">
+        <Router>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={home} />
+              <Route exact path="/login" component={login} />
+              <Route exact path="/signup" component={signup} />
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    </MuiThemeProvider>
+  );
+}
+
+export default App;
