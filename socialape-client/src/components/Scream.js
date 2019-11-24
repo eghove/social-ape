8c1b1d3cf13f5caa1fe36ed2fc29de7dc86824d5
That_Guy <eghove@gmail.com>
2019-11-24 14:59:14 -0600

    setting up login page

--- socialape-client/src/components/Scream.js
+++ socialape-client/src/components/Scream.js
@@ -4,15 +4,27 @@ import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
+import { Link } from "react-router-dom";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
 
 const styles = {
   card: {
-    display: "flex"
+    display: "flex",
+    marginBottom: 20
+  },
+  image: {
+    minWidth: 200
+  },
+  content: {
+    padding: 25,
+    objectFit: "cover"
   }
 };
 
 class Scream extends Component {
   render() {
+    dayjs.extend(relativeTime);
     const {
       classes,
       scream: {
@@ -26,16 +38,25 @@ class Scream extends Component {
       }
     } = this.props;
 
-    console.log(createdAt);
-
     return (
       <div>
-        <Card>
-          <CardMedia image={userImage} title="Profile image" />
-          <CardContent>
-            <Typography variant="h5">{userHandle}</Typography>
-            <Typography variant = "body2">{createdAt}</Typography>
-            <Typography variant = "body1">{body}</Typography>
+        <Card className={classes.card}>
+          <CardMedia
+            image={userImage}
+            title="Profile image"
+            className={classes.image}
+          />
+          <CardContent className={classes.content}>
+            <Typography
+              variant="h5"
+              component={Link}
+              to={`/users/${userHandle}`}
+              color="primary"
+            >
+              {userHandle}
+            </Typography>
+            <Typography variant="body2">{dayjs(createdAt).fromNow()}</Typography>
+            <Typography variant="body1">{body}</Typography>
           </CardContent>
         </Card>
       </div>
